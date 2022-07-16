fe90b95e479b4d675ebac4772976e60c690a2a83
Adnan Khan <adnan.x.khan@gmail.com>
2022-07-15 19:41:10 -0500

    initial commit

---
+++ src/main/java/com/akhan/hwk8s/ekshelloworld/EkshelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.akhan.hwk8s.ekshelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkshelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkshelloworldApplication.class, args);
+	}
+
+}
